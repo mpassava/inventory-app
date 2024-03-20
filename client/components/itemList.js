fc936e14faacdd73a08cfa659a4cd3dba23bdb47
mpassava <mpassava@gmail.com>
2024-03-20 17:10:56 -0400

    added basic layout for app

--- client/components/itemList.js
+++ client/components/itemList.js
@@ -1,6 +1,18 @@
-export default function createItemList() {
-  const div = document.createElement("div");
-  div.setAttribute("id", "itemList");
-  div.innerText = "This is where the item list will go";
-  document.body.append(div);
+export default function renderItems() {
+  const item = `
+  <tr>
+    <td class="border px-4 py-2">1</td>
+    <td class="border px-4 py-2">Product 1</td>
+    <td class="border px-4 py-2">Category A</td>
+    <td class="border px-4 py-2">10</td>
+    <td class="border px-4 py-2">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">
+        Edit
+      </button>
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded">
+        Delete
+      </button>
+    </td>
+  </tr>
+  `;
 }
