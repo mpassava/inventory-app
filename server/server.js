6abadea6171d6d98b1e63af530595f0efbcba416
mpassava <mpassava@gmail.com>
2024-03-20 14:24:42 -0400

    added db connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const sqlite3 = require('sqlite3');
 const app = express();
 const port = 3030;
+
+app.use(express.json());
+
+const db = new sqlite3.Database('inventorydb.sqlite3', (err) => {
+  if (err) {
+    console.error(err.message, 'error connecting to database');
+    return;
+  }
+  console.log('database connection established')
+})
 
 app.get("/", (req, res) => {
   res.send("App is working");
@@ -13,3 +24,14 @@ app.listen(port, (err) => {
   }
   console.log(`server listening on port ${port}`);
 });
+
+process.on('SIGINT', () => {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message, 'error closing database')
+      return;
+    }
+    console.log('database connection closed');
+    process.exit(0);
+  })
+})
