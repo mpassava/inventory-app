1550574df354187dccb12d0fd35642126fe5d5ac
mpassava <mpassava@gmail.com>
2024-03-21 15:34:59 -0400

    added item list headers

--- client/components/itemList.js
+++ client/components/itemList.js
@@ -1,10 +1,10 @@
 export default function renderItems() {
   const item = `
   <tr>
-    <td class="border px-4 py-2">1</td>
-    <td class="border px-4 py-2">Product 1</td>
-    <td class="border px-4 py-2">Category A</td>
-    <td class="border px-4 py-2">10</td>
+    <td class="border px-4 py-2"></td>
+    <td class="border px-4 py-2"></td>
+    <td class="border px-4 py-2"></td>
+    <td class="border px-4 py-2"></td>
     <td class="border px-4 py-2">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded">
         Edit
