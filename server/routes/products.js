0f6b2a4cf797586d86f298f4b142ff953fff0466
mpassava <mpassava@gmail.com>
2024-07-22 20:55:05 -0400

    created /headers endpoint

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const DBManager = require("../db.js");
+
 const DB = DBManager.getInstance();
 
-router.get("/", async (_, res) => {
+router.get("/", (_, res) => {
   DB.connect();
 
-  DB.all("SELECT * FROM Products", {}, (err, rows) => {
+  DB.all("SELECT * FROM Products", [], (err, rows) => {
     if (err) {
       res.status(500).send("server error fetching data");
+      return;
     }
     res.status(200).json(rows);
   });
 
   DB.close();
 });
+
+router.get('/headers', (_, res) => {
+  DB.connect();
+
+  DB.all("PRAGMA table_info(Products)", [], (err, rows) => {
+    if (err) {
+      res.status(500).send('server error fetching table data');
+      return;
+    }
+
+    const headers = rows.map(row => row.name);
+    res.status(200).json(headers);
+  })
+})
 
 module.exports = router;
--- server/seed.js
+++ server/seed.js
@@ -3,27 +3,36 @@ const sqlite3 = require("sqlite3");
 const data = require("./lib/sampleData.json");
 
 const db = new sqlite3.Database("inventorydb.sqlite3");
+console.log("database connection established");
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS Products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
+    category TEXT,
+    product_name TEXT,
     description TEXT,
-    price REAL,
-    category TEXT
+    purchase_price REAL,
+    sales_price REAL,
+    margin INT,
+    location TEXT
   )`);
 
+
+
   const stmt = db.prepare(
-    `INSERT INTO Products (id, name, description, price, category) VALUES (?,?,?,?,?)`
+    `INSERT INTO Products (id, category, product_name, description, purchase_price, sales_price, margin, location) VALUES (?,?,?,?,?,?,?,?)`
   );
   data.forEach((product) => {
     stmt.run(
       [
         product.id,
-        product.name,
-        product.description,
-        product.price,
         product.category,
+        product.product_name,
+        product.description,
+        product.purchase_price,
+        product.sales_price,
+        product.margin,
+        product.location,
       ],
       (err) => {
         if (err) console.error(err.message);
